4386c59032cb4fb7206f2bc8cec3409e2c6799e9
TiagoSagara <tiago.as.eaa@gmail.com>
2026-02-23 18:12:24 -0300

    custom_appbar & themes

---
+++ lib/utils/image_list.dart
@@ -0,0 +1,6 @@
+abstract final class ImageList {
+  static const logoMultSistem = "assets/images/logo.png";
+  static const carouselAzul = "assets/images/carrossel_azul.png";
+  static const carouselBolha = "assets/images/carrossel_bolha.png";
+  static const carouselBoliche = "assets/images/carrossel_boliche.png";
+}
